f2c97cee8b0fa3465b21de6f549163e314371263
matheussatoshi1 <matheussdev3@gmail.com>
2025-01-13 01:49:41 -0400

    chore: update jest config to use tests directory instead of src

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
 import { Config } from "jest";
 
 const jestConfig: Config = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.{test|spec}.{ts,tsx}"],
+  roots: ["<rootDir>/tests"],
+  collectCoverageFrom: ["<rootDir>/tests/**/*.{test|spec}.{ts,tsx}"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
