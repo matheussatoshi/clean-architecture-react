7618eefdc68fdeab0fa19f45d6d02abea0a725ca
matheussatoshi1 <matheussdev3@gmail.com>
2025-01-25 00:58:26 -0400

    chore: clear project files and folders

--- src/data/test/mock-http-post.ts
+++
@@ -1,8 +0,0 @@
-import type { HttpPostParams } from "@/data/protocols/http/http-post-client";
-import { mockAxiosData } from "@/infra/test";
-import { faker } from "@faker-js/faker";
-
-export const mockPostRequest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: mockAxiosData,
-});
--- src/domain/errors/invalid-credentials-error.ts
+++
@@ -1,6 +0,0 @@
-export class InvalidCredentialsError extends Error {
-  constructor() {
-    super("Credênciais Inválidas");
-    this.name = "InvalidCredentialsError";
-  }
-}
