4e95f59e12948e492c63e705ba5914e35fc5f74b
matheussatoshi1 <matheussdev3@gmail.com>
2025-01-27 00:59:08 -0400

    feat: adds https status codes

--- src/data/protocols/http-client.protocol.ts
+++ src/data/protocols/http-client.protocol.ts
@@ -30,3 +30,15 @@ export type MakeWithBodyHttpRequestParams = {
   body: any;
   params?: HttpParams;
 };
+
+export enum HttpStatus {
+  OK = 200,
+  CREATED = 201,
+  NOT_CONTENT = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  TOO_MANY_REQUESTS = 429,
+  INTERNAL_SERVER = 500,
+}
