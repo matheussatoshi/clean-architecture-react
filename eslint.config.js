530ed00e9dda1bfcdb367c20173ed1d9bbcce1ec
matheussatoshi1 <matheussdev3@gmail.com>
2025-01-27 23:58:13 -0400

    chore: allow TypeScript namespaces by disabling @typescript-eslint/no-namespace

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,7 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/no-namespace": "off",
     },
   },
 );
