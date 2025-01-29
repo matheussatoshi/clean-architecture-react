ca306d31f6a2d3f3307cf4be85a1ca4d723bee99
matheussatoshi1 <matheussdev3@gmail.com>
2025-01-28 20:04:43 -0400

    chore: remove env.d.ts file

--- interfaces/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    VITE_DEV_BASE_URL: string;
-    VITE_PROD_BASE_URL: string;
-    VITE_MODE: "production" | "devolopment";
-    VITE_API_TIMEOUT: number;
-  }
-}
