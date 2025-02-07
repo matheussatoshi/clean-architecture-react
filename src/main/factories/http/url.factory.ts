48f6006d26a3229cb480218f358bb550f697b7dc
matheussatoshi1 <matheussdev3@gmail.com>
2025-02-07 08:56:33 -0400

    refactor: simplify URL generation logic

--- src/main/factories/http/url.factory.ts
+++ src/main/factories/http/url.factory.ts
@@ -3,11 +3,9 @@ const DOMAIN = window.location.hostname.includes("localhost");
 
 const host = MODE || DOMAIN;
 
-const getURL = (): string => {
+const makeURL = (): string => {
   if (host) return import.meta.env.VITE_DEV_BASE_URL;
   return import.meta.env.VITE_PROD_BASE_URL;
 };
-
-const makeURL = (): string => getURL();
 
 export { host, makeURL };
