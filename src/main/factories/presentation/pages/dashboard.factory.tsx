1017cdeb1970c73d98c4bda4c1ed0ab45d0dede1
matheussatoshi1 <matheussdev3@gmail.com>
2025-02-07 08:58:09 -0400

    feat: update DashboardPage with welcome message and create factory

---
+++ src/main/factories/presentation/pages/dashboard.factory.tsx
@@ -0,0 +1,5 @@
+import { Page } from "@/presentation/pages";
+
+export const makeDashboard = (): JSX.Element => {
+  return <Page.Dashboard />;
+};
--- src/presentation/pages/dashboard/dashboard.page.tsx
+++ src/presentation/pages/dashboard/dashboard.page.tsx
@@ -1,7 +1,7 @@
 export default function DashboardPage() {
   return (
     <div className="h-full w-full bg-neutral-50 flex items-center justify-center">
-      Bem vindo!
+      <h1 className="text-3xl text-center">Bem vindo!</h1>
     </div>
   );
 }
